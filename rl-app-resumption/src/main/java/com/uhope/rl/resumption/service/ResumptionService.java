eec18189fb50fadf6c8f135e356fb08c7cbc382a
StivenYang <StivenYang626@gmail.com>
2018-10-30 17:31:39 +0800

    分析统计模块：修复部分角色登录后显示不正确的问题

--- rl-app-resumption/src/main/java/com/uhope/rl/resumption/service/ResumptionService.java
+++ rl-app-resumption/src/main/java/com/uhope/rl/resumption/service/ResumptionService.java
@@ -16,14 +16,14 @@ public interface ResumptionService {
      * @param currentGrade 河长等级
      * @return
      */
-    List<ReachPatrolNumStatisticDTO> findReachNeedPatrolNumStatistic(Integer type,String regionId,Integer intervalMonths, Integer currentGrade,Integer pageNumber,Integer pageSize);
+    List<ReachPatrolNumStatisticDTO> findReachNeedPatrolNumStatistic(Integer type,Integer intervalMonths, Integer currentGrade,Integer pageNumber,Integer pageSize);
 
     /**
      * 以区为单位查找各级河长已巡次数
      * @param currentGrade 河长等级
      * @return
      */
-    List<ReachPatrolNumStatisticDTO> findReachHadPatrolNumStatistic(Integer type,String regionId,String startTime, String endTime, Integer currentGrade,Integer pageNumber,Integer pageSize);
+    List<ReachPatrolNumStatisticDTO> findReachHadPatrolNumStatistic(Integer type,String startTime, String endTime, Integer currentGrade,Integer pageNumber,Integer pageSize);
 
     /**
      * 根据条件查询指定区下面的各个等级河长的巡查情况
