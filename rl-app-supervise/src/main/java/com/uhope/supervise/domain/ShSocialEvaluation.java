1efb3a9ba235454e8e29371eaffc43e4870ef54e
StivenYang <StivenYang626@gmail.com>
2018-11-23 17:26:55 +0800

    完成部分社会监督满意度调查后台接口修改

--- rl-app-supervise/src/main/java/com/uhope/supervise/domain/ShSocialEvaluation.java
+++ rl-app-supervise/src/main/java/com/uhope/supervise/domain/ShSocialEvaluation.java
@@ -1,7 +1,6 @@
 package com.uhope.supervise.domain;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -18,35 +17,90 @@ import java.util.Date;
  */
 @Data
 public class ShSocialEvaluation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SELECT REPLACE(UUID(),\"-\",\"\")")
+    /**
+     * id
+     */
     private String id;
 
+    /**
+     * 期号
+     */
     private String termNumber;
 
+    /**
+     * 行政区域
+     */
     private String regionName;
 
+    /**
+     * 水域名称
+     */
     private String riverName;
 
+    /**
+     * 区级河长
+     */
+    private String districtChairman;
+
+    /**
+     * 镇街级河长
+     */
+    private String townChairman;
+
+    /**
+     * 监督员姓名
+     */
     private String supervisor;
 
+    /**
+     * 评价时间
+     */
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
     @JSONField(format="yyyy-MM-dd HH:mm")
     private Date evaluationDate;
 
+    /**
+     * 是否满意
+     */
     private Integer isSatisfied;
 
+    /**
+     * 不满意原因
+     */
     private String dissatisfiedReason;
 
+    /**
+     * 其他说明
+     */
     private String otherReason;
 
+    /**
+     * 现场照片
+     */
     private String problemPics;
 
-    private String problemPosition;
-
+    /**
+     * 联系方式
+     */
     private String contactType;
 
-    private transient String creator;
+    /**
+     * 创建人
+     */
+    private String creator;
 
-    private transient Date createTime;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 具体位置
+     */
+    private String problemPosition;
 }
