3928333446bff1b09781efcf24ffc6f3bae7a00e
shujihui <13523328703@163.com>
2018-12-10 14:16:04 +0800

    修改权限查询问题

--- rl-app-water/src/main/java/com/uhope/water/web/SurfaceWaterController.java
+++ rl-app-water/src/main/java/com/uhope/water/web/SurfaceWaterController.java
@@ -63,7 +63,12 @@ public class SurfaceWaterController {
     @GetMapping("/list")
     public Result<PageInfo<SurfaceWater>> list(@RequestParam(defaultValue = Constant.DEFAULT_PAGE_NUMBER) Integer pageNumber,
                                                @RequestParam(defaultValue = Constant.DEFAULT_PAGE_SIZE) Integer pageSize,
-                                               String issue, String status, String createUser,Integer num) {
+                                               @RequestParam String objectid,
+                                               String issue, String status, String createUser) {
+        String num="";
+        if("市河长办".equals(surfaceWaterService.selectRole(objectid))){
+            num="2";
+        }
         return ResponseMsgUtil.success(surfaceWaterService.list(pageNumber,pageSize,issue,status,createUser,num));
     }
 
